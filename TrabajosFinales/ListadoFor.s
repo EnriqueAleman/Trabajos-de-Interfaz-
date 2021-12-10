7cf894cb05fe8947a5c61497de63fa36e33b9c3a
Echo <89487826+EnriqueAleman@users.noreply.github.com>
2021-12-09 19:34:19 -0800

    Add files via upload

---
+++ TrabajosFinales/ListadoFor.s
@@ -0,0 +1,21 @@
+##//  Autor :  EnriqueAleman
+##//  Institucion   :  Instituto Tecnologico de Tijuana
+##//  Catedratico   :  Rene Solis Reyes 
+##//  Fecha : 12/09/21
+##//  Nombre del Ejercicio  :   For While
+##//  18210450
+##//  Descripcion del problema: Traducción del if
+
+//Este codigo funcionara como un Listado//
+ldr 	r1,	 = a
+ldr	r1,	 [ r1 ]
+ldr	r2,	 =b
+ldr 	r2, 	[ r2 ]
+cmp r1, r2
+bne sino
+entonces :
+
+b final
+sino :
+//Aqui terminaria el codigo//
+final : ...
